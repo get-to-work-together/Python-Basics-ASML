6fe490d3882c942f21ef70de875d4983135bf8a0
Peter Anema <email@peteranema.nl>
2025-10-14 21:42:48 +0200

    Update

--- Training/demo_pandas.py
+++ Training/demo_pandas.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
+#%%
 filename = r'/Users/peter/Computrain/_InCompany/ASML/Python Basics/Sandbox/ca-500.csv'
 df = pd.read_csv(filename, sep=';')
 
+#%%
 print(df.info())
 
-# print(df[['first_name', 'last_name']].head(20))
-
-
-print(df)
+print(df[['first_name', 'last_name']].head(20))
