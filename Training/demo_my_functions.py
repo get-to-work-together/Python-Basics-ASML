1fcd9ed05ad1624d19453863af513aac40781665
Peter Anema <email@peteranema.nl>
2026-09-07 09:28:39 +0200

    Update

--- Training/demo_my_functions.py
+++
@@ -1,3 +0,0 @@
-import my_functions
-
-my_functions.print_banner('YEAAAAHHH!!!')
--- Training/exercise sort on vowels.py
+++
@@ -1,8 +0,0 @@
-
-words = 'the quick fox jumped over the lazy brown dog abracadabra banana'.split()
-
-sorted_words = sorted(words, key = lambda word: sum(word.count(v) for v in 'aeiou'))
-print(sorted_words)
-
-sorted_words = sorted(words, key = lambda word: word[::-1])
-print(sorted_words)
